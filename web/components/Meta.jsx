9cc98c9d79c27025b6bac818625f3a84302b3171
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2022-04-23 16:52:59 +0530

    lol

--- web/components/Meta.jsx
+++ web/components/Meta.jsx
@@ -9,6 +9,8 @@ export default function Meta({ title, desc }) {
       <meta name="description" content={desc} />
       <meta name="robots" content="index, follow" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta name="og:title" content={title} />
+      <meta name="og:desc" content={desc} />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
