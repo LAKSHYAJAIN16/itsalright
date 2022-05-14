b4653db04db51f3c871e8fcbe8382c1f30ffe4f5
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2022-05-14 14:07:21 +0530

    lol

--- web/pages/home.js
+++ web/pages/home.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
+import Script from "next/script";
 
+import Meta from "../components/Meta";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
@@ -63,14 +65,28 @@ export default function Home() {
 
   return (
     <div onMouseDown={(e) => scrollFailSafe(e)}>
-      <Head>
-        <title>Its alright : Share your problems and get Solutions</title>
-        <meta
-          name="description"
-          content="Get access to a rich community of therapists, doctors, motivational speakers, etc, pledging to correct your mistakes and helping you out"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Meta
+        title={"Itsalright : Together, we can make it alright"}
+        desc={
+          "Get access to a free body of thousands of mental health experts and get their advice for free"
+        }
+      />
+
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-5QG3T5J6L6`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-5QG3T5J6L6', {
+        page_path: window.location.pathname,
+        });
+        `}
+      </Script>
 
       <div className={styles.main}>
         <Navbar />
