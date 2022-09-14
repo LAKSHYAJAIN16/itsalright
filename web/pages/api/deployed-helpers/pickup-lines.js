ae3af46b05be56ac376ae8214996cac8966a74db
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2022-09-14 20:18:04 +0530

    feat: added survey

--- web/pages/api/deployed-helpers/pickup-lines.js
+++ web/pages/api/deployed-helpers/pickup-lines.js
@@ -1,33 +1,28 @@
 import { updateDoc, doc, getDoc, setDoc } from "firebase/firestore";
-import NextCors from "nextjs-cors";
 
 import { db } from "../../../lib/firebase";
 
 export default async function handler(req, res) {
-  await NextCors(req, res, {
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
-    optionsSuccessStatus: 200,
-  });
-
+  const id = req.body.id;
   const data = {
     line: req.body.line,
     id: req.body.id,
     scores: req.body.scores,
   };
 
-  const doc2 = await getDoc(doc(db, "pickup-lines", data.id));
+  const actDoc = doc(db, "pickup-lines", id)
+  const doc2 = await getDoc(actDoc);
 
   if (doc2.exists()) {
     const data2 = doc2.data();
     const scores = data2.scores;
     const newScores = scores.concat(data.scores);
-    await updateDoc(doc(db, "pickup-lines", data.id), {
+    await updateDoc(doc(db, "pickup-lines", id), {
       scores: newScores,
     });
     res.send("done method 2");
   } else {
-    await setDoc(doc(db, "pickup-lines", data.id), data);
+    await setDoc(doc(db, "pickup-lines", id), data);
     res.send("done method 1");
   }
 }
